13042860370ea8acab7c291ed3a19730512db1c6
Iva Abadjieva <iva_abadjieva@hotmail.com>
2020-02-19 20:14:20 +0100

    Aenderung in der Git-Variante

--- src/main/java/com/mkyong/SpringBootWebApplication.java
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+//UNTER GIT
+
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
