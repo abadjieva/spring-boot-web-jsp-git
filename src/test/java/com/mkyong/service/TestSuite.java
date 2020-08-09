0d32393894a7ad7a06c7fd2dcabfd89cff33abd1
Iva Abadjieva <iva_abadjieva@hotmail.com>
2020-08-09 15:00:27 +0200

    Suite Test

---
+++ src/test/java/com/mkyong/service/TestSuite.java
@@ -0,0 +1,11 @@
+package com.mkyong.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ServiceTest.class, ServiceTestMock.class} )
+public class TestSuite {
+
+
+}
